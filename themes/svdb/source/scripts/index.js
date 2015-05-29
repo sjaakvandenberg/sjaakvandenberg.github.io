f733c8be96ff0d17d7ac93070b06e37339ca6133
sjaakvandenberg <mail@svdb.co>
2015-05-29 19:48:39 +0200

    Nuke

---
+++ themes/svdb/source/scripts/index.js
@@ -0,0 +1,15 @@
+/**
+ * Main JS file for theme behaviours
+ */
+
+/*globals jQuery, document */
+(function ($) {
+    "use strict";
+
+    $(document).ready(function(){
+
+        $(".post-content").fitVids();
+
+    });
+
+}(jQuery));
